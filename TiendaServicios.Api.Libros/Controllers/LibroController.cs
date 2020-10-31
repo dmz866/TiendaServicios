0ad44d893358661042049f0fccc3f8220b59b63a
David Murillo <dmz866@hotmail.com>
2020-10-30 23:40:56 -0500

    libro getlist

--- TiendaServicios.Api.Libros/Controllers/LibroController.cs
+++ TiendaServicios.Api.Libros/Controllers/LibroController.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using TiendaServicios.Api.Libros.Aplicacion.Dtos;
 using TiendaServicios.Api.Libros.Aplicacion.LibroHandler;
 
 namespace TiendaServicios.Api.Libros.Controllers
@@ -13,6 +15,11 @@ namespace TiendaServicios.Api.Libros.Controllers
         public LibroController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<LibroDto>>> GetList()
+        {
+            return await _mediator.Send(new LibroGetList.Request());
         }
         [HttpPost]
         public async Task<ActionResult<Unit>> Insert(LibroInsert.Request data)
